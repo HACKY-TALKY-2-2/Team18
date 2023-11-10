e354e7d094061f8494f2267c85f560e7069e466a
ohsung0722 <99doldol@gmail.com>
2023-11-11 04:56:01 +0900

    [UPDATE] css수정

--- front/src/components/photo.js
+++ front/src/components/photo.js
@@ -43,80 +43,85 @@ function Photo() {
   };
 
   return (
-    <div className="couple-container">
-      <div className="photo-image-container">
-        <div className="text-input-container">
+    <div className="SimRes">
+      <div className="couple-container">
+        <div className="photo-image-container">
+          <div className="text-input-container">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="이름 1"
+              aria-label="default input example"
+              value={textFirstInput}
+              onChange={handleTextFirstChange}
+            />
+          </div>
           <input
-            className="form-control"
-            type="text"
-            placeholder="이름 1"
-            aria-label="default input example"
-            value={textFirstInput}
-            onChange={handleTextFirstChange}
+            type="file"
+            accept="image/*"
+            style={{ display: "none" }}
+            onChange={(e) => handleImageChange(e, 0)}
+            ref={fileInputRefs[0]}
           />
+          {images[0].previewImage ? (
+            <img src={images[0].previewImage} alt="Preview 1" className="img" />
+          ) : (
+            <img
+              src={`https://via.placeholder.com/300`}
+              alt="Example 1"
+              className="img"
+            />
+          )}
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => handleButtonClick(0)}
+          >
+            이미지 업로드
+          </button>
         </div>
-        <input
-          type="file"
-          accept="image/*"
-          style={{ display: "none" }}
-          onChange={(e) => handleImageChange(e, 0)}
-          ref={fileInputRefs[0]}
-        />
-        {images[0].previewImage ? (
-          <img src={images[0].previewImage} alt="Preview 1" className="img" />
-        ) : (
-          <img
-            src={`https://via.placeholder.com/300`}
-            alt="Example 1"
-            className="img"
-          />
-        )}
-        <button
-          type="button"
-          className="btn btn-success"
-          onClick={() => handleButtonClick(0)}
-        >
-          이미지 업로드
-        </button>
-      </div>
-      <div className="similarity">
-        <h6>얼마나 닮았을까?</h6>
-      </div>
-      <div className="photo-image-container">
-        <div className="text-input-container">
+        <div className="similarity">
+          <h6>얼마나 닮았을까?</h6>
+        </div>
+        <div className="photo-image-container">
+          <div className="text-input-container">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="이름 2"
+              aria-label="default input example"
+              value={textSecondInput}
+              onChange={handleTextSecondChange}
+            />
+          </div>
           <input
-            className="form-control"
-            type="text"
-            placeholder="이름 2"
-            aria-label="default input example"
-            value={textSecondInput}
-            onChange={handleTextSecondChange}
+            type="file"
+            accept="image/*"
+            style={{ display: "none" }}
+            onChange={(e) => handleImageChange(e, 1)}
+            ref={fileInputRefs[1]}
           />
+          {images[1].previewImage ? (
+            <img src={images[1].previewImage} alt="Preview 2" className="img" />
+          ) : (
+            <img
+              src={`https://via.placeholder.com/300`}
+              alt="Example 2"
+              className="img"
+            />
+          )}
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => handleButtonClick(1)}
+          >
+            이미지 업로드
+          </button>
         </div>
-        <input
-          type="file"
-          accept="image/*"
-          style={{ display: "none" }}
-          onChange={(e) => handleImageChange(e, 1)}
-          ref={fileInputRefs[1]}
-        />
-        {images[1].previewImage ? (
-          <img src={images[1].previewImage} alt="Preview 2" className="img" />
-        ) : (
-          <img
-            src={`https://via.placeholder.com/300`}
-            alt="Example 2"
-            className="img"
-          />
-        )}
-        <button
-          type="button"
-          className="btn btn-success"
-          onClick={() => handleButtonClick(1)}
-        >
-          이미지 업로드
-        </button>
       </div>
+      <button type="button" className="btn btn-success">
+        등록하기
+      </button>
     </div>
   );
 }
--- front/src/components/similarLogo.js
+++ front/src/components/similarLogo.js
@@ -3,10 +3,12 @@ import "../css/startBox.css";
 
 function DrawSimilarLogo() {
   return (
-    <div className="title">
-      <div className="serviceLogo">
-        <h1>자기야, 나 사랑해?? 얼만큼?</h1>
-        <h5>사랑하면 닮는다던데......</h5>
+    <div className="SimRes">
+      <div className="title">
+        <div className="serviceLogo">
+          <h1>자기야, 나 사랑해?? 얼만큼?</h1>
+          <h5>사랑하면 닮는다던데......</h5>
+        </div>
       </div>
     </div>
   );
