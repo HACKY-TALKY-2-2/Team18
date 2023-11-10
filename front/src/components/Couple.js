0b61b3569e31e58a0357a7962381a8776d75b10a
oganessone <oganessone718@gmail.com>
2023-11-11 03:34:52 +0900

    fix css to merge

--- front/src/components/Couple.js
+++ front/src/components/Couple.js
@@ -4,16 +4,16 @@ import "bootstrap";
 function Couple({props}) {
   console.log(props);
   return (
-    <div className="SR-couple-container">
-      <div className="SR-image-container">
-        <img src={"./example.png"} alt="img1" className="SR-img"></img>
+    <div className="couple-container">
+      <div className="image-container">
+        <img src={"./example.png"} alt="img1" className="img"></img>
         <h6 className="SR-name">{props.name1}</h6>
       </div>
-      <div className="SR-similarity">
+      <div className="similarity">
         <h6>{props.similarity}%</h6>
       </div>
-      <div className="SR-image-container">
-        <img src={"./example.png"} alt="img1" className="SR-img"></img>
+      <div className="image-container">
+        <img src={"./example.png"} alt="img1" className="img"></img>
         <h6 className="SR-name">{props.name2}</h6>
       </div>
     </div>
