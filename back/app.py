e3a036285913aa34166133af14d4ee9f7e506141
InfDev00 <119485240+InfDev00@users.noreply.github.com>
2023-11-11 05:48:38 +0900

    flask 완성 - localhost

--- back/app.py
+++ back/app.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, jsonify, send_from_directory
 import doppleganger
+import face_comparison
+import face_for_single
 import os
 
 
 app = Flask(__name__)
 
+url = "http://localhost:5000/upload/"
 app.config['UPLOAD_FOLDER'] = 'resource/uploads'
 app.config['RESOURCE_FOLDER'] = 'resource/neutral'
 
-@app.route('/upload/<filename>', methods=['GET'])
-def uploaded_file(filename):
-    return send_from_directory(app.config['RESOURCE_FOLDER'], filename)
+@app.route('/upload/<filename>/<state>', methods=['GET'])
+def uploaded_file(filename, state=0):
+    if state == 0:
+        return send_from_directory(app.config['RESOURCE_FOLDER'], filename)
+    else:
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-@app.route('/compare', methods=['POST'])
-def upload_images():
+@app.route('/single', methods=['POST'])
+def upload_single():
     try:
         if 'file' not in request.files:
             return jsonify({'error': 'No file part'})
@@ -23,37 +29,62 @@ def upload_images():
         if file.filename == '':
             return jsonify({'error': 'No selected file'})
 
-        upload_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        
-        # 이미지를 업로드 경로에 저장
+        upload_path = os.path.join(app.config['UPLOAD_FOLDER'],"face.jpg")
         file.save(upload_path)
 
-        # 업로드된 이미지의 경로를 반환
-        return jsonify({'image_path': upload_path, 'filename': file.filename})
+        return jsonify({'image_path': upload_path, 'filename': "face.jpg"})
 
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+@app.route('/single/symmetric', methods=['GET'])
+def get_symmetric():
+    path = os.path.join(app.config['UPLOAD_FOLDER'],"face.jpg")
+    score = face_for_single.symmetric(path)
+
+    return jsonify({"score":score,'image_url': url + "face.jpg" +'/1'})
+
+@app.route('/compare', methods=['POST'])
+def upload_images():
+    try:
+        if 'file1' not in request.files or 'file2' not in request.files:
+            return jsonify({'error': 'No file part'})
+
+        file1 = request.files['file1']
+        file2 = request.files['file2']
+        if file1.filename == '' or file2.filename == '':
+            return jsonify({'error': 'No selected file'})
+
+        upload_path = os.path.join(app.config['UPLOAD_FOLDER'], "fst.jpg")
+        file1.save(upload_path)
+        upload_path = os.path.join(app.config['UPLOAD_FOLDER'], "snd.jpg")
+        file2.save(upload_path)
+        return jsonify({"file1_name":"fst.jpg", "file2_name":"snd.jpg"})
+    except:
+        return jsonify({"error": "Error saving"})
+
+@app.route('/compare/similarity', methods=['GET'])
+def compare_similarity():
+    try:
+        sim = face_comparison.face_compare(app.config['UPLOAD_FOLDER'])
+
+        return jsonify({'sim': sim, 'fst_url': url+'fst.jpg/1', 'snd_url': url+'snd.jpg/1'})
     except Exception as e:
         return jsonify({'error': str(e)})
 
 @app.route('/dopple', methods=['POST'])
 def upload_image():
     try:
-        # 이미지 파일을 받음
         if 'file' not in request.files:
             return jsonify({'error': 'No file part'})
 
         file = request.files['file']
-
-        # 파일이 없을 경우 예외 처리
         if file.filename == '':
             return jsonify({'error': 'No selected file'})
 
-        # 이미지를 업로드할 경로 설정
         upload_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        
-        # 이미지를 업로드 경로에 저장
         file.save(upload_path)
 
-        # 업로드된 이미지의 경로를 반환
         return jsonify({'image_path': upload_path, 'filename': file.filename})
 
     except Exception as e:
@@ -65,7 +96,7 @@ def get_dopple(filename):
         img_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         sim, dopple_path = doppleganger.find_doppleganger(img_path, app.config['RESOURCE_FOLDER'])
 
-        return jsonify({'sim': sim, 'image_url': 'http://localhost:5000/upload/' + dopple_path})
+        return jsonify({'sim': sim, 'image_url': url + dopple_path+'/0'})
     except Exception as e:
         return jsonify({'error': str(e)})
 
--- back/face_comparison.py
+++ back/face_comparison.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import face_recognition
 
-threshold = 0.6
-def face_compare(fst_img:str = 'obama', snd_img:str = 'obama2'):
-    fst_np_img = face_recognition.load_image_file(f"resource/Images/{fst_img}.jpg")
-    snd_np_img = face_recognition.load_image_file(f"resource/Images/{snd_img}.jpg")
+
+def face_compare(img_dir:str ="resource/Images"):
+    fst_np_img = face_recognition.load_image_file(img_dir+"/fst.jpg")
+    snd_np_img = face_recognition.load_image_file(img_dir+"/snd.jpg")
 
     # Get the face encodings for the known images
     fst_face_encoding = face_recognition.face_encodings(fst_np_img)[0]
@@ -13,6 +13,4 @@ def face_compare(fst_img:str = 'obama', snd_img:str = 'obama2'):
     # See how far apart the test image is from the known faces
     face_distance = face_recognition.face_distance([fst_face_encoding], snd_face_encoding)[0]
 
-    print(f"유사도는 {round((1-face_distance)*100, 2)}% 입니다.")
-    if face_distance < threshold: print("닮으셨네요")
-    else: print("안 닮았네요")
+    return 1-face_distance
